6837e64f00e98a42838f9986c7c6078d82c7610a
jb <j_builes@hotmail.com>
2025-10-23 14:54:35 -0500

    commit v1

--- management.Application/obj/Debug/net8.0/management.Application.AssemblyInfo.cs
+++ management.Application/obj/Debug/net8.0/management.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("management.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9750f8ab85856d942175ad83f7381001e8d712b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c457c5098565a9832666a460c0e6bf0b03b227c")]
 [assembly: System.Reflection.AssemblyProductAttribute("management.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("management.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
