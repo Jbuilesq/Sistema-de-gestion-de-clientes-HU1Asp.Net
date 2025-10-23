bf969d0f20a1cef59238a956bbc7d31fc690ec9c
jb <j_builes@hotmail.com>
2025-10-23 08:41:01 -0500

    commit entitys

---
+++ management.Domain/Entitys/Order.cs
@@ -0,0 +1,13 @@
+namespace management.Domain.Entitys;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustumerId { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+    public string Status  { get; set; } = "Pending";
+    
+    public Customer customer { get; set; }
+    
+    
+}
