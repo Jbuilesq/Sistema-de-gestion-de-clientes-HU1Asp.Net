59f1863ee3e0dc7c56bf80a720f1f2a23edb8067
DavidRendonDev <davidrex114@hotmail.com>
2025-10-24 08:21:20 -0500

    feat: add cutomer model

---
+++ management.Domain/Entitys/Customer.cs
@@ -0,0 +1,8 @@
+namespace management.Domain.Entitys;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public string Email { get; set; } 
+}
